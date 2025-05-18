26e085f3d9b8f818ec4b6febd025da5673ef9f87
Shah Ehsan <shahehsan2002@gmail.com>
2025-05-18 17:24:00 +0600

    boolean

---
+++ bolean.js
@@ -0,0 +1,4 @@
+// Boolean values in JavaScript
+// Boolean values can be either true or false
+const isMarried = true;
+console.log(isMarried);
--- string.js
+++ string.js
@@ -1,2 +1,5 @@
+// String values in JavaScript
+// String values are used to represent text
+// String values can be enclosed in single quotes, double quotes, or backticks
 const name = "Ehsan";
 console.log(name);
