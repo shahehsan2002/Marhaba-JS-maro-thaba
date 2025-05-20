03ce221c28e6933bf55927882eb4c19319adb44c
Shah Ehsan <shahehsan2002@gmail.com>
2025-05-20 23:14:45 +0600

    boolean explore

--- boolean.js
+++ boolean.js
@@ -2,5 +2,20 @@
 // Boolean values can be either true or false
 const isMarried = true;
 const isSingle = false;
+// Boolean values are often used in logical operations
+const isAdult = true;
+const isStudent = false;
+// Boolean values can be used in comparisons
+const isEqual = (5 === 5); // true
+const isNotEqual = (5 !== 10); // true
+// Boolean values can be used in logical operations
+const isTrue = (isAdult && isStudent); // false
+const isFalse = (isAdult || isStudent); // true
+// Boolean values can be used in conditional statements
+if (isMarried) {
+  console.log("You are married.");
+} else {
+  console.log("You are not married.");
+}
 // Boolean values are often used in conditional statements
 console.log(isMarried);
